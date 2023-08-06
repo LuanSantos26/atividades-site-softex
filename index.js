d7fd947b413d1abcadc13e9d993d89323cafeaa7
LuanSantos26 <luansantos26@outlook.com>
2023-08-06 12:34:55 -0300

    calculadora.js

--- index.js
+++ index.js
@@ -1,29 +1,68 @@
-function calculadora(num1, num2, operacao) {
-  // Verificar se a operação é válida (1 a 4)
-  if (operacao < 1 || operacao > 4) {
-    return 0;
-  }
+const prompt = require('prompt-sync')();
 
-  // Realizar a operação correspondente
-  switch (operacao) {
-    case 1:
-      return num1 + num2;
-    case 2:
-      return num1 - num2;
-    case 3:
-      return num1 * num2;
-    case 4:
-      // Verificar se o divisor não é zero antes da divisão
-      return num2 !== 0 ? num1 / num2 : 0;
-  }
+
+function calculadora() {
+
+        let resultado = 0;
+        let operacao = prompt("Informe a operação (1: Soma 2: Subtração 3: Multiplicação 4: Divisão 0: Sair): ");
+        let numero1 = "";
+        let numero2 = "";
+
+        
+        if (operacao != 0) {
+
+            switch (operacao) {
+                case "1":
+                    console.log("Adição Selecionada!");
+                    numero1 = parseFloat(prompt("Informe o 1º número: "));    
+                    numero2 = parseFloat(prompt("Informe o 2º número: "));
+                    resultado = numero1 + numero2;
+                    console.log(`O resultado da soma é: ${resultado}`);
+                    calculadora();
+                    break;    
+                    
+                case "2":
+                    console.log("Subtração Selecionada!");
+                    numero1 = parseFloat(prompt("Informe o 1º número: "));    
+                    numero2 = parseFloat(prompt("Informe o 2º número: "));
+                    resultado = numero1 - numero2;
+                    console.log(`O resultado da subtração é: ${resultado}`);
+                    calculadora();
+                    break;         
+                   
+                case "3":
+                    console.log("Multiplicação Selecionada!");
+                    numero1 = parseFloat(prompt("Informe o 1º número: "));    
+                    numero2 = parseFloat(prompt("Informe o 2º número: "));
+                    resultado = numero1 * numero2;
+                    console.log(`O resultado da Multiplicação é: ${resultado}`);
+                    calculadora();
+                    break;         
+                    
+                case "4":
+                    console.log("Divisão Selecionada!");
+                    numero1 = parseFloat(prompt("Informe o 1º número: "));    
+                    numero2 = parseFloat(prompt("Informe o 2º número: "));
+                    resultado = numero1 / numero2;
+                    console.log(`O resultado da Divisão é: ${resultado}`);
+                    calculadora();
+                    break; 
+    
+                case "0":
+                    console.log("Saindo!");
+                    break;        
+                    
+                default:
+                    console.log("Essa opção não existe!");
+                    calculadora();
+                    break;             
+             }
+            
+        } else {
+            console.log("Saindo!");
+        }
+        
+
+          
 }
-
-// Exemplos de uso da função:
-let num1 = console.log("digte um numero: ");
-num1=parseInt;
-let num2 = console.log("digite um seguro numero: ");
-num=parseInt;
-let operacao = console.log("digite o numero da operacao sendo 1 para soma, 2 para subtracao, 3 multiplicacao e 4 para divisao.");
-operacao=parseInt; // 1: Soma, 2: Subtração, 3: Multiplicação, 4: Divisão
-let resultado = calculadora(num1,num2, operacao);
-console.log(resultado); // Exibe o resultado da operação
+calculadora();
