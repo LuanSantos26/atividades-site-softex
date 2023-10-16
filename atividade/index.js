7e52c3a1f358b8277376b0208a613da27c768659
LuanSantos26 <luansantos26@outlook.com>
2023-10-16 16:08:59 -0300

    .

---
+++ atividade/index.js
@@ -0,0 +1,19 @@
+class Produto {
+
+    constructor(){
+        this.id=0;
+        this.nomeProduto="";
+        this.valor=0;
+
+    }
+
+    adiconar(){
+        alert('Vamos adicionar um veiculo!')
+    }
+
+    excluir(){
+        alert('Você excluiu um Veiculo da lista!')
+    }
+}
+
+var produto = new produto();
