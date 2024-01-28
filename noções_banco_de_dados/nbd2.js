c14f27bff34170cc9e058dddfb296b849914bc3e
LuanSantos26 <luansantos26@outlook.com>
2024-01-28 18:37:52 -0300

    nbd2.js

---
+++ noções_banco_de_dados/nbd2.js
@@ -0,0 +1,11 @@
+-- Inserção do produto 1
+INSERT INTO Produtos (NomeProduto, Preco, QuantidadeEstoque)
+VALUES ('Smartphone', 799.99, 20);
+
+-- Inserção do produto 2
+INSERT INTO Produtos (NomeProduto, Preco, QuantidadeEstoque)
+VALUES ('Tablet', 349.99, 10);
+
+-- Inserção do produto 3
+INSERT INTO Produtos (NomeProduto, Preco, QuantidadeEstoque)
+VALUES ('Fone de Ouvido', 49.99, 50);
